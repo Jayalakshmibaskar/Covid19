9f420626fa147aef0a38ce16ce40e180d1664090
Jayalakshmibaskar <jayalakshmibaskar99@gmail.com>
2020-04-12 21:51:17 +0530

    getUser updated

--- getOrders.php
+++ getOrders.php
@@ -4,38 +4,27 @@ if(!$conn){
     die("Connection failed".mysqli_connect_error());
 }
 else{
+    $userid=$_GET['userid'];
     $obj=(object)null;
     $output=array();
     $output1=array();
     $myObj=(object)null;
-    $select="select * from orders";
+    $select="select * from orders where UserId='$userid'";
     $result= mysqli_query($conn,$select);
+    $test=array();
     if(mysqli_num_rows($result)>0){
-    while($row=mysqli_fetch_object($result)){
-        // array_push($output,$row->OrderId);
-        
-        // array_push($output1,$row->Orders);
-        // json_encode($output1,JSON_UNESCAPED_SLASHES);
-        $orderId =$row->OrderId;
-        $orders = $row->Orders;       
+    while($row=mysqli_fetch_assoc($result)){
+        $orderId =$row['OrderId'];
+        $orders = $row['Orders'];  
         $obj->orderId=$orderId;        
         $newOrders = str_replace(array("\r\n", "\r", "\n"), "", $orders); 
         $norders=json_decode($newOrders);
         $obj->orders=$norders;
-        array_push($output,$obj);
-    
+       
+    array_push($output,json_encode($obj));
         
-        //$myObj=(object)null;
-
-    }
-        // $myObj->orderid = $output;
-        // $myobj->Orders=$output1;     
-        $myJSON = json_encode($output);
-        
-        //$str=preg_replace('/\\\"/',"\"",$myJSON);
-        echo $myJSON;
-    
-    //echo json_encode($output,JSON_UNESCAPED_SLASHES);
+    }        
+        echo stripslashes(json_encode($output));       
     }
 }
 mysqli_close($conn);
